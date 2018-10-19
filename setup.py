f6ba4b222971fa017ab85b353a5492531e879cfd
Egon Kidmose <egk@es.aau.dk>
2018-10-19 11:34:36 +0200

    initial commit. All credit to Nipun Batra. Minor edit by committer.
    https://nipunbatra.github.io/blog/2014/latexify.html

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ppfl',
+      version='0.0.1',
+      description='pyplot for LaTeX',
+      url='http://github.com/kidmose/ppfl',
+      author='Nipun Batra',
+      license='unspecified',
+      packages=['ppfl'],
+      zip_safe=False)
